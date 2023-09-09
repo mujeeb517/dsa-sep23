e465eb589e2a239b6e906d698e59f5b3323bddee
Mujib <mujib517@icloud.com>
2023-09-09 12:41:02 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,78 @@
+import javax.sound.midi.SysexMessage;
+import java.math.BigDecimal;
+
+public class Main {
+    // 100 files
+    // variables and data types
+    public static void main(String[] args) {
+        // strongly typed
+        // LHS, RHS
+        // declaring, init
+        // byte 8bits, short 16, int 32, long 64
+        // 32 bit
+        // 16bit = 2^16
+        // 32 bit = 2^32
+        // 64bit
+        // 2^32 => 2^10. 2^10. 2^10. 2^2
+        // 10^3.10^3.10^3
+        // 10^9: 100000000
+        // constraints
+        // 0<=n<=10^18
+        // IOT
+        // 64kb
+        // os, your program, variables
+        // 16GB
+        // float: 32 bit
+        // double: 64bit
+        // 2^0 2^1 2^2 2^2
+        // 2^-2 2^-1 2^0 2^1
+        // primitive
+        // byte, short, int, long,bigInt
+        // float, double, bigDecimal
+        // char, string
+        char c = '&'; // 2 bytes
+        c = '*';
+        // a - 97
+        // image
+        // matrix
+        // numbers
+        // pixels
+        // color -> value rgb
+        // beautify
+        // down 3L
+        // learning
+        // fundamental operations
+        // format
+        // operate
+
+//        String name = "John \n" +
+//                " is \n" +
+//                "in engineering\n";
+//
+//        System.out.println(name);
+
+
+        int x = 10;
+        int y = 3;
+
+        // arithmatic: +, -, *, /, % binary
+        // conditional operators: <,>,<=,>=,!=, ==
+        // logical operators: &&, ||, !
+        // bitwise &, |, ^, >>, <<
+        // conditional statement
+        // loops
+        // class, object, inheritance
+        // recursion
+        // ternary operator
+        // number
+        // operands,
+        // boolean 1 byte
+        // 0000 0000 0000 0001
+
+//        int z = x %  y;
+
+        int val = 1;
+        int n = 10;
+        System.out.println((1 << n));
+    }
+}
